0b6f2d0db2f1d11f19ecc89a9d186628423e9acd
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-01-11 16:37:40 +0100

    v0.0.3

--- build/components/Example/index.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-interface Props {
-}
-declare const Example: React.FunctionComponent<Props>;
-export default Example;
--- build/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Example from './components/Example';
-export { Example };
-export declare function sum(a: number, b: number): number;
